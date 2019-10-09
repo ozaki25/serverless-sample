1f958cde3327003168f61e0fa2f0945f5cd0d72d
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-10 08:00:46 +0900

    add cors setting

--- handler.js
+++ handler.js
@@ -1,6 +1,9 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.get('/hello', (req, res) => {
   res.send('Hello');
